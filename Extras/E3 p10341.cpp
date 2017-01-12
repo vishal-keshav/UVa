267120c6aec6740b39cb50bd7b14ac7ec2406788
bulletcross <bulletcross@gmail.com>
2017-01-12 23:21:20 +0530

    10341 AC

--- Extras/E3 p10341.cpp
+++ Extras/E3 p10341.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <algorithm>
-
+#include <iostream>
 using namespace std;
 
 int p,q,r,s,t,u;
@@ -17,9 +17,7 @@ bool same_sign(double a, double b){
 int main(){
 	double left, middle, right;
 	double left_ans,middle_ans,right_ans;
-	char line[2000];
-
-	while(sscanf(gets(line), "%d %d %d %d %d %d",&p,&q,&r,&s,&t,&u)!=EOF){
+	while (cin >> p >> q >> r >> s >> t >> u){ //Had to use cin because there was some problem with EOF detection
         left_ans = func(0);
         right_ans = func(1);
         left =0;
@@ -28,7 +26,7 @@ int main(){
             printf("No solution\n");
         }
         else{
-            while(right-left>=0.0001){
+            while(right-left>=0.000000001){
                 middle = (right-left)/2 + left;
                 middle_ans = func(middle);
                 if(same_sign(left_ans,middle_ans)){
@@ -38,8 +36,13 @@ int main(){
                     right = middle;
                 }
             }
-            printf("%.4lf\n",middle);
+            if(abs(func(middle)<=0.0001)){
+                printf("%.4lf\n",middle);
+            }
+            else{
+                printf("No solution\n");
+            }
         }
 	}
-	return 0;
+	return(0);
 }
