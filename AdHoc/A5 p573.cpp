bfa0210027468923b27536ac5b3a17a25da71a9a
bulletcross <bulletcross@gmail.com>
2017-01-23 22:25:26 +0530

    573 AC

--- AdHoc/A5 p573.cpp
+++ AdHoc/A5 p573.cpp
@@ -4,24 +4,34 @@ using namespace std;
 
 int main(){
 	int H,U,D,F,days;
-	float dist,fatigue;
+	float dist,fatigue,last_climb;
 	cin >> H >> U >> D >> F;
 	while(H){
 		days = 0;
 		fatigue = (float)(F*U)/100;
-		dist = U - D;
-
-		while(dist < H && dist >= 0){
-			days++;
-			if(dist - days*fatigue + U >= 0){
-				dist += U - days*fatigue;
-			}
-			if(dist>H){
-				break;
-			}
-			dist -= D;
-
+		if(U>H){
+			dist+=U;
 		}
+		else{
+			dist = U - D;
+			last_climb = U;
+			while(dist < H && dist >= 0){
+				days++;
+				if(last_climb-fatigue >= 0){
+					dist += last_climb-fatigue;
+					last_climb = last_climb - fatigue;
+				}
+				else{
+					last_climb = 0;
+				}
+				if(dist>H){
+					break;
+				}
+				dist -= D;
+
+			}
+		}
+		
 		if(dist>0){
 			cout << "success on day " << days+1 << endl;
 		}
