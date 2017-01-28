77c846aa7a562d0cc53a315d6641f6a2f2bc79e8
bulletcross <bulletcross@gmail.com>
2017-01-28 20:19:19 +0530

    837 AC

--- AdHoc/A8 p837.cpp
+++ AdHoc/A8 p837.cpp
@@ -18,8 +18,9 @@ bool sort_func(struct elem e1, struct elem e2){
 
 int main(){
 	int T,N;
-	double x1,y1,x2,y2,c;
+	double x1,y1,x2,y2,c,swap_elem;
 	cin >> T;
+	//freopen("output.txt","w",stdout);
 	while(T--){
 		cin >> N;
 		vector<struct elem> vec;
@@ -28,14 +29,35 @@ int main(){
 			cin >> x1 >> y1 >> x2 >> y2 >> c;
 			//Preserve values
 			if(x1>x2){
-				y1 = x1;
+				swap_elem = x1;
 				x1 = x2;
-				x2 = y1;
+				x2 = swap_elem;
+				swap_elem = y1;
+				y1 = y2;
+				y2 = swap_elem;
 			}
-			temp_elem.start_x = x1;
-			temp_elem.end_x = x2;
-			temp_elem.coeff = c;
-			vec.push_back(temp_elem);
+			//Logic for remove negative line segment
+            if(y1<=0 && y2 <=0 && (y1+y2)<0){
+                continue;
+            }
+            else if(y1>=0 && y2>=0){
+                temp_elem.start_x = x1;
+                temp_elem.end_x = x2;
+                temp_elem.coeff = c;
+                vec.push_back(temp_elem);
+            }
+            else{
+                if(y1>=0){
+                    x2 = x1 - ((x1-x1)/(y2-y1))*y1;
+                }
+                else{
+                    x1 = x1 - ((x1-x1)/(y2-y1))*y1;
+                }
+                temp_elem.start_x = x1;
+                temp_elem.end_x = x2;
+                temp_elem.coeff = c;
+                vec.push_back(temp_elem);
+            }
 		}
 		//Sort vec
 		sort(vec.begin(),vec.end(),sort_func);
@@ -49,6 +71,7 @@ int main(){
 		vector<struct elem>::iterator temp_iter;
 		double combined_coeff,min_end_x;
 		vector<struct elem> ans_vec;
+		vector<struct elem> ans_vec_n;
 		struct elem ans_elem;
 		while(vec.size()!=0){
 			iter = vec.begin();
@@ -62,7 +85,7 @@ int main(){
 				temp_iter++;
 			}
 			if(temp_iter!=vec.end()){
-				min_end_x = min(min_end_x,(*temp_iter).start_x);	
+				min_end_x = min(min_end_x,(*temp_iter).start_x);
 			}
 			temp_iter--;
 			while(temp_iter!=iter){
@@ -86,14 +109,30 @@ int main(){
 			ans_elem.coeff = combined_coeff;
 			ans_vec.push_back(ans_elem);
 		}
+		//Fill the ans for gaps where there are no coefficient
+		for(iter=ans_vec.begin(); (iter+1)!=ans_vec.end(); ++iter){
+			if((*iter).end_x == (*(iter+1)).start_x){
+                ans_vec_n.push_back((*iter));
+			}
+			else{
+                ans_elem.start_x = (*iter).end_x;
+                ans_elem.end_x = (*(iter+1)).start_x;
+                ans_elem.coeff = 1.000;
+                ans_vec_n.push_back((*iter));
+                ans_vec_n.push_back(ans_elem);
+			}
+		}
+		ans_vec_n.push_back((*iter));
 		//Start printing ans
-		
 		combined_coeff = 1;
-		printf("%d\n",ans_vec.size()+2);
-		printf("-inf %.3f %.3f\n",ans_vec[0].start_x,combined_coeff);
-		for(vector<struct elem>::iterator it=ans_vec.begin(); it!=ans_vec.end(); ++it){
+		printf("%d\n",ans_vec_n.size()+2);
+		printf("-inf %.3f %.3f\n",ans_vec_n[0].start_x,combined_coeff);
+		for(vector<struct elem>::iterator it=ans_vec_n.begin(); it!=ans_vec_n.end(); ++it){
 			printf("%.3f %.3f %.3f\n", (*it).start_x, (*it).end_x, (*it).coeff);
 		}
-		printf("%.3f +inf %.3f\n\n",ans_vec[ans_vec.size()-1].end_x, combined_coeff);
+		printf("%.3f +inf %.3f\n",ans_vec_n[ans_vec_n.size()-1].end_x, combined_coeff);
+		if(T!=0){
+			printf("\n");
+		}
 	}
 }
