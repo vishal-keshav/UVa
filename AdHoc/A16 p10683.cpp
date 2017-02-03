2a219581165bb0bc588463ba046cefc9619067e9
bulletcross <bulletcross@gmail.com>
2017-02-03 21:21:22 +0530

    10683 WA

---
+++ AdHoc/A16 p10683.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+	unsigned long long int input,time;
+	long double fraction = 10000000.0/8640000.0;
+
+	while(cin >> input){
+        int zeros = 0;
+		time = 0;
+		time+=360000*(input/1000000);
+		input = input%1000000;
+		time+=6000*(input/10000);
+		input = input%10000;
+		time+=100*(input/100);
+		input = input%100;
+		time+=input;
+        input = time*fraction;
+        while(input){
+            input = input/10;
+            zeros++;
+        }
+        zeros = 6-zeros;
+        for(int i=0;i<zeros;i++){
+            cout << "0";
+        }
+		cout << (int)(time*fraction) << endl;
+	}
+	return 0;
+}
