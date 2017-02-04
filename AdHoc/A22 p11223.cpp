bff5384f37ed2c097116dda6b11f7d5507ff8820
bulletcross <bulletcross@gmail.com>
2017-02-04 13:53:25 +0530

    11223 AC

--- AdHoc/A22 p11223.cpp
+++ AdHoc/A22 p11223.cpp
@@ -2,6 +2,7 @@
 #include <map>
 #include <string>
 #include <sstream>
+#include <stdio.h>
 
 using namespace std;
 
@@ -13,16 +14,17 @@ string code[53] = {".-","-...","-.-.","-..",".","..-.", "--.","....","..",\
 					"---...","-.-.-.","-...-",".-.-.","-....-","..--.-",".-..-.",".--.-."};
 char symbol[53] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R',\
 					'S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9',\
-					'.',',','?','\'','!','/','(',')','&',':',';','=','+','-','_','"','@'};
-map<string,char> hash_map;
+					'.',',','?','\'','!','/','(',')','&',':',';','=','+','-','_','\"','@'};
+map<string,char> my_hash_map;
 
 void init_hash_map(void){
 	for(int i=0;i<53;i++){
-		hash_map[code[i]] = symbol[i];
+		my_hash_map[code[i]] = symbol[i];
 	}
 }
 
 int main(){
+    //freopen("output.txt","w",stdout);
 	int T,iter=1;
 	string line;
 	cin >> T;
@@ -35,16 +37,18 @@ int main(){
 		stringstream ss(line);
 		cout << "Message #" << iter << endl;
 		while(ss >> word){
-            cout << hash_map[word];
+            cout << my_hash_map[word];
             index+=word.length();
             index++;
-            if(line[index] == ' '){
+            if(index < line.length() && line[index] == ' '){
                 cout << " ";
                 index++;
             }
-
 		}
 		cout << endl;
+		if(T!=0){
+            cout << endl;
+		}
 		iter++;
 	}
 	return 0;
