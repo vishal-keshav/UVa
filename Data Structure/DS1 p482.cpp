2d15fd4d732942011092c1163955929a0d49100d
bulletcross <bulletcross@gmail.com>
2017-02-12 15:18:14 +0530

    482 WA

---
+++ Data Structure/DS1 p482.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <stdio.h>
+
+using namespace std;
+
+int main(){
+	int T,N,iter=0;
+	string input,word;
+	char permu_word;
+	cin >> T;
+
+	cin.ignore();
+	while(T--){
+        getline(cin,input);
+		iter = 0;
+		vector<int> permutation;
+		vector<string> output;
+
+		getline(cin,input);
+		stringstream ss(input);
+		while(ss >> word){
+			permutation.push_back(word[0] - '0');
+			iter++;
+		}
+		/*Converting permutaion in linear format*/
+		vector<int> new_permu(iter);
+		for(int i=0;i<iter;i++){
+            new_permu[permutation[i]-1] = i;
+		}
+
+		getline(cin,input);
+		stringstream sss(input);
+		while(sss >> word){
+			output.push_back(word);
+		}
+
+		for(vector<int>::iterator iter = new_permu.begin(); iter!=new_permu.end();iter++){
+			cout << output[(*iter)] << endl;
+		}
+		if(T!=0){
+            cout << endl;
+		}
+	}
+	return 0;
+}
