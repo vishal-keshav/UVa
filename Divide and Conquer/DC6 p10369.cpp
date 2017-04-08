9441e80d0443be4f753cfc189f1c70649988901c
bulletcross <bulletcross@gmail.com>
2017-04-08 14:39:55 +0530

    10369 AC

--- Divide and Conquer/DC6 p10369.cpp
+++ Divide and Conquer/DC6 p10369.cpp
@@ -6,14 +6,14 @@
 using namespace std;
 
 struct outpost{
-	int x, y;
+	double x, y;
 };
 
 struct outpost camps[501];
 
 bool visited[501];
 int S,P;
-int temp_D;
+double D_temp;
 
 void init_graph(void){
 	for(int i=0;i<P;i++){
@@ -21,8 +21,8 @@ void init_graph(void){
 	}
 }
 
-int dist(int n1, int n2){
-	return sqrt(pow(camps[n1].x-camps[n2].x,2)+pow(camps[n1].y-camps[n2].y,2));
+double dist(int n1, int n2){
+	return sqrt(pow(camps[n1].x-camps[n2].x,2.0)+pow(camps[n1].y-camps[n2].y,2.0));
 }
 
 void visit_dfs_way(int node){
@@ -37,8 +37,9 @@ void visit_dfs_way(int node){
 	}
 }
 
-int nr_sub_graphs(void){
+int nr_sub_graphs(double d){
 	int sub_graph = 0;
+	D_temp = d;
 	for(int i=0;i<P;i++){
 		if(!visited[i]){
 			visit_dfs_way(i);
@@ -48,7 +49,55 @@ int nr_sub_graphs(void){
 	return sub_graph;
 }
 
+double absolute(double a, double b){
+	if(a<b){
+		return (b-a);
+	}
+	else{
+		return (a-b);
+	}
+}
+
 int main(){
-	
+    //freopen("output.txt","w",stdout);
+    int T;
+    cin >> T;
+    while(T--){
+		cin >> S >> P;
+		for(int i=0;i<P;i++){
+			cin >> camps[i].x >> camps[i].y;
+		}
+
+		double left = 0.00;
+		double right = 10000.00;
+		double mid;
+		double prev_mid=0.00;
+		int nr_graphs;
+		while(1){
+			init_graph();
+			mid = (left+right)/2;
+			nr_graphs = nr_sub_graphs(mid);
+			if(nr_graphs == S){
+				//Check if nothing can be done
+				if(absolute(mid,prev_mid) < 0.0001){
+					break;
+				}
+				else{
+					prev_mid = mid;
+					right = mid;
+
+				}
+			}
+			else if(nr_graphs < S){
+				prev_mid = mid;
+				right = mid;
+			}
+			else{
+				prev_mid = mid;
+				left = mid;
+			}
+		}
+		printf("%0.2f\n",mid);
+    }
 	return 0;
 }
