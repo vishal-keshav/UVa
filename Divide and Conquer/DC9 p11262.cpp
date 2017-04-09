a9da7220a37eb8392407b92f8ed30ae28ec15632
bulletcross <bulletcross@gmail.com>
2017-04-09 13:29:07 +0530

    11262 TL due to inefficient version of MBP

--- Divide and Conquer/DC9 p11262.cpp
+++ Divide and Conquer/DC9 p11262.cpp
@@ -2,8 +2,11 @@
 #include <stdio.h>
 #include <vector>
 #include <algorithm>
+#include <math.h>
 
 using namespace std;
+
+//#define DEBUG 1
 
 struct pole{
 	int x, y;
@@ -11,7 +14,7 @@ struct pole{
 
 int P,k;
 int P_blue, P_red;
-int mid_temp;
+double mid_temp;
 
 struct pole blue[101];
 struct pole red[101];
@@ -28,12 +31,50 @@ void init(void){
 	}
 }
 
-int get_nr_fences(int d){
+double dist(int blue_idx, int red_idx){
+	double ret=sqrt(pow(blue[blue_idx].x-red[red_idx].x,2) + pow(blue[blue_idx].y-red[red_idx].y,2));
+#ifdef DEBUG
+//	cout << "Distance between blue "<< blue_idx << " and red " << red_idx << " = "<< ret << endl;
+#endif
+	return ret;
+}
+
+int max_fences(int blue_idx){
+#ifdef DEBUG
+	cout << "max_fences call for " << blue_idx << endl;
+#endif
+	//Base case
+	if(blue_idx == P_blue){
+        //cout << "Base condition" << endl;
+		return 0;
+	}
+	//Main case
+	else{
+		int ret = 0;
+		//Try connecting with possible reds
+		for(int i=0;i<P_red;i++){
+			if(!red_covered[i] && dist(blue_idx,i) <= mid_temp){
+				red_covered[i] = true;
+				ret = max(ret,1+max_fences(blue_idx+1));
+				red_covered[i] = false;
+			}
+		}
+		//Dont try connecting this blue
+		ret = max(ret, max_fences(blue_idx+1));
+#ifdef DEBUG
+//		cout << "max_fences for " << blue_idx << " returns " << ret << endl;
+#endif
+		return ret;
+	}
+}
+
+int get_nr_fences(double d){
 	mid_temp = d;
-	
+	return max_fences(0);
 }
 
 int main(){
+    //freopen("output.txt","w",stdout);
 	int T,temp_x,temp_y;
 	string color;
 	cin >> T;
@@ -54,31 +95,50 @@ int main(){
 				P_red++;
 			}
 		}
+		if(min(P_blue,P_red)<k){
+			cout << "Impossible" << endl;
+			continue;
+		}
 		init();
-		int left = 0;
-		int right = 2000;
-		int mid;
+		double left = 0.0;
+		double right = 2000.00;
+		double mid;
 		int prev_mid = 0;
+		int nr_fences;
 		while(1){
+#ifdef DEBUG
+//		cout << "In loop" << endl;
+#endif
 			mid = (left + right)/2;
 			nr_fences = get_nr_fences(mid);
+#ifdef DEBUG
+			cout << "mid is " << mid << ". nr_fences is " << nr_fences << endl;
+#endif
 			if(nr_fences == k){
-				if(prev_mid-mid==0){
+				if(right-left<0.1){
 					break;
 				}
 				else{
 					right = mid;
+					prev_mid = (int)mid;
 				}
 			}
 			else if(nr_fences > k){
-				left = mid;
+				right = mid;
+				prev_mid = (int)mid;
 			}
 			else{
-				right = mid;
+				left = mid;
+				prev_mid = (int)mid;
 			}
 		}
-		
+		if(get_nr_fences((double)floor(mid))==k){
+            cout << floor(mid) << endl;
+		}
+		else{
+            cout << ceil(mid) << endl;
+		}
 	}
-	
+
 	return 0;
 }
