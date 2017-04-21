9f3cc25cef5bf0845143c0a340178a04731d51e0
bulletcross <bulletcross@gmail.com>
2017-04-21 23:39:38 +0530

    11054 AC

--- Greedy/GD7 p11054.cpp
+++ Greedy/GD7 p11054.cpp
@@ -2,19 +2,76 @@
 #include <stdio.h>
 #include <algorithm>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
+struct winer{
+	long long int nr_wine;
+	long long int location;
+};
+
+long long int absolute(long long int a){
+	if(a<0){
+		return (0-a);
+	}
+	else{
+		return a;
+	}
+}
+
 int main(){
-	int n,cost,left,right,reduction;
+	long long int n,cost,left,right,reduction;
 	cin >> n;
 	while(n){
-		vector<int> input(n);
-		for(int i=0;i<n;i++){
-			cin >> input[i];
+		//vector<int> input(n);
+		queue<struct winer> buyer;
+		queue<struct winer> seller;
+		long long int input;
+		for(long long int i=0;i<n;i++){
+			cin >> input;
+			struct winer temp;
+			if(input>0){
+				temp.nr_wine = input;
+				temp.location = i;
+				buyer.push(temp);
+			}
+			if(input<0){
+				temp.nr_wine = -input;
+				temp.location = i;
+				seller.push(temp);
+			}
 		}
 		cost = 0;
-		for(int i=0;i<n;i++){
+		//Keep popping out buyers from far left and sellers from far left to meet demand
+		struct winer temp_buyer;
+		struct winer temp_seller;
+		while(!buyer.empty()){
+			temp_buyer = buyer.front();
+			//pick a seller from far left
+			temp_seller = seller.front();
+
+			//Case 1: seller completely satisfy buyer
+			if(temp_buyer.nr_wine == temp_seller.nr_wine){
+				cost+=temp_buyer.nr_wine*absolute(temp_buyer.location-temp_seller.location);
+				buyer.pop();
+				seller.pop();
+			}
+			//buyer is satisfied, the seller has more to sell
+			else if(temp_buyer.nr_wine < temp_seller.nr_wine){
+				cost+=temp_buyer.nr_wine*absolute(temp_buyer.location-temp_seller.location);
+				seller.front().nr_wine-= temp_buyer.nr_wine;
+				buyer.pop();
+			}
+			//seller is done, but buyer wants more
+			else{
+				cost+=temp_seller.nr_wine*absolute(temp_buyer.location-temp_seller.location);
+				seller.pop();
+				buyer.front().nr_wine-=temp_seller.nr_wine;
+			}
+		}
+
+		/*for(int i=0;i<n;i++){
 			left = i;
 			right = i;
 			if(input[i]<0){
@@ -37,7 +94,7 @@ int main(){
 					cost+=reduction*(right-i);
 				}
 			}
-		}
+		}*/
 		cout << cost << endl;
 		cin >> n;
 	}
