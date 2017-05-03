92de723694739d69b7a11394d2f42210c1892c8c
bulletcross <bulletcross@gmail.com>
2017-05-03 23:09:46 +0530

    11790 continued

---
+++ Dynamic Programming/DP8 p11790.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+	int T,N,nr_case=1;
+	cin >> T;
+	while(T--){
+		cin >> N;
+		vector<int> height(N);
+		vector<int> width(N);
+		for(int i=0;i<N;i++){
+			cin >> height[i];
+		}
+		for(int i=0;i<N;i++){
+			cin >> width[i];
+		}
+		//Processing
+		
+	}
+	
+	return 0;
+}
