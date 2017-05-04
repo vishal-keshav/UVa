feb5cc66fd9ed5a764b242e15136bc9eb98d7aca
bulletcross <bulletcross@gmail.com>
2017-05-04 19:19:30 +0530

    11790 AC

--- Dynamic Programming/DP8 p11790.cpp
+++ Dynamic Programming/DP8 p11790.cpp
@@ -18,9 +18,57 @@ int main(){
 		for(int i=0;i<N;i++){
 			cin >> width[i];
 		}
-		//Processing
-		
+		//Processing for lis
+		vector<int> lis(N);
+		int max_lis = 0;
+		for(int i=0;i<N;i++){
+			lis[i] = width[i];
+			if(lis[i]>max_lis){
+				max_lis = lis[i];
+			}
+		}
+		for(int i=1;i<N;i++){
+			for(int j=0;j<i;j++){
+				if(height[i]>height[j]){
+					if(lis[i]<lis[j]+width[i]){
+						lis[i] = lis[j] + width[i];
+						if(lis[i]>max_lis){
+							max_lis = lis[i];
+						}
+					}
+				}
+			}
+		}
+		//Processing for lds
+		vector<int> lds(N);
+		int max_lds = 0;
+		for(int i=0;i<N;i++){
+			lds[i] = width[i];
+			if(lds[i]>max_lds){
+				max_lds = lds[i];
+			}
+		}
+		for(int i=1;i<N;i++){
+			for(int j=0;j<i;j++){
+				if(height[i]<height[j]){
+					if(lds[i]<lds[j]+width[i]){
+						lds[i] = lds[j] + width[i];
+						if(lds[i]>max_lds){
+							max_lds = lds[i];
+						}
+					}
+				}
+			}
+		}
+		cout << "Case " << nr_case << ". ";
+		if(max_lis>=max_lds){
+			cout << "Increasing (" << max_lis << "). Decreasing (" << max_lds << ")." << endl;
+		}
+		else{
+			cout << "Decreasing (" << max_lds << "). Increasing (" << max_lis << ")." << endl;
+		}
+		nr_case++;
 	}
-	
+
 	return 0;
 }
