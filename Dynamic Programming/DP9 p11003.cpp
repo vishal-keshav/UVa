7ba3de452d2e783d59828316494fc0738b117718
bulletcross <bulletcross@gmail.com>
2017-05-05 22:01:37 +0530

    11003 TL

--- Dynamic Programming/DP9 p11003.cpp
+++ Dynamic Programming/DP9 p11003.cpp
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+#define INT_MAXIMUM 100000000
+
 int main(){
 	int N;
 	cin >> N;
@@ -14,26 +16,38 @@ int main(){
 		for(int i=0;i<N;i++){
 			cin >> weight[i] >> load[i];
 		}
-		vector<int> lis_load(N);
-		vector<int> lis(N,1);
-		int max_lis = 1;
-		for(int i=0;i<N;i++){
-			lis_load[i] = 0;
+		vector<vector<int> > DP(N+1,vector<int>(N+1,INT_MAXIMUM));
+		//Fill the initial arrays
+		for(int i=0;i<=N;i++){
+			DP[0][i] = INT_MAXIMUM;
+			DP[i][0] = 0;
 		}
-		for(int i=1;i<N;i++){
-			for(int j=0;j<i;j++){
-				if(weight[i]+lis_load[j] < load[i]){
-					if(lis[i]<1+lis[j]){
-						lis[i] = 1 + lis[j];
-						lis_load[i] = lis_load[j] + weight[i];
-						if(lis[i] > max_lis){
-							max_lis = lis[i];
+		//Fill the DP, retaining max_len
+		int max_len = 1;
+		for(int i=1,index=N-1;i<=N,index>=0;i++,index--){
+            //cout << "Processing for " << weight[index] << endl;
+			for(int j=1;j<=N;j++){
+				for(int k=i-1;k>=0;k--){
+					if(weight[index]+DP[k][j-1]<DP[i][j]){
+						if(DP[k][j-1]<=load[index]){
+							DP[i][j] = weight[index]+DP[k][j-1];
+							if(max_len<j){
+								max_len = j;
+							}
 						}
 					}
 				}
 			}
 		}
-		cout << max_lis << endl;
+
+		/*for(int i=0;i<=N;i++){
+			for(int j=0;j<=N;j++){
+				cout << DP[i][j] << "  ";
+			}
+			cout << endl;
+		}*/
+		cout << max_len << endl;
+
 		cin >> N;
 	}
 
