d073e05463d569c904a8210fd81b82db5d2d40b7
bulletcross <bulletcross@gmail.com>
2017-05-14 18:46:17 +0530

    166 WA

--- Dynamic Programming/DP12 p166.cpp
+++ Dynamic Programming/DP12 p166.cpp
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+#define MAXIMUM_INT 10000000
+
 int deno[6] = {1,2,4,10,20,40};
 
 int main(){
@@ -37,8 +39,36 @@ int main(){
 			}
 		}
 		//Find the possibility of all exchanges, select with min coin exchange
-		
-		
+		vector<int> payable(max_amount+1,MAXIMUM_INT);
+		payable[0] = 0;
+		for(int i=0;i<6;i++){
+			for(int j=0;j<nr_coin[i];j++){
+				for(int k=1;k<=max_amount;k++){
+					if(k-deno[i]>=0 && payable[k-deno[i]]!=MAXIMUM_INT){
+						if(payable[k]>1+payable[k-deno[i]]){
+							payable[k] = 1+payable[k-deno[i]];
+						}
+					}
+				}
+			}
+		}
+		//Iterate to get the minimum exchange
+		int res = MAXIMUM_INT;
+		for(int i=1;i<=max_amount;i++){
+			if(payable[i]==1){
+				if(i-pay_int >=0){
+					if(res>payable[i]+value_exchange[i-pay_int]){
+						res = payable[i]+value_exchange[i-pay_int];
+					}
+				}
+			}
+		}
+		cout << res << endl;
+		max_amount = 0;
+		for(int i=0;i<6;i++){
+			cin >> nr_coin[i];
+			max_amount+=deno[i]*nr_coin[i];
+		}
 	}
 	return 0;
 }
