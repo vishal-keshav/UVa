44bf776f86111758bd34b0e4094de47f370d476c
bulletcross <bulletcross@gmail.com>
2017-05-20 23:28:06 +0530

    If it all goes wrong... Darling, just hold on...

--- Dynamic Programming/DP16 p10313.cpp
+++ Dynamic Programming/DP16 p10313.cpp
@@ -17,14 +17,31 @@ int main(){
 		while(ss >> input_int[index]){
 			index++;
 		}
+		vector<int> denom(input_int[0]);
+		for(int i=0;i<input_int[0];i++){
+			denom[i] = i+1;
+		}
+		int ret;
 		//Case 1: Only N
 		if(index==1){
 			N = input_int[0];
+			vector<int> DP(N+1,0);
+			DP[0] = 1;
+			for(int i=0;i<N;i++){
+				for(int j=1;j<=N;j++){
+					if(j-denom[i]>=0){
+						DP[j]+=DP[j-denom[i]];
+					}
+				}
+			}
+			ret = DP[N];
 		}
 		//Case 2: N and L1
 		else if(index==2){
 			N = input_int[0];
 			L1 = input_int[1];
+			vector<vector<int> > DP(N+1,vector<int>(L1+1,0));
+			
 		}
 		//Case 3: N, L1, L2
 		else{
