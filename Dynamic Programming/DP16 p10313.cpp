43b69c1c1160f8753e190eadee23297a981a20c0
bulletcross <bulletcross@gmail.com>
2017-05-20 23:29:18 +0530

    10313 TL :(

--- Dynamic Programming/DP16 p10313.cpp
+++ Dynamic Programming/DP16 p10313.cpp
@@ -21,7 +21,7 @@ int main(){
 		for(int i=0;i<input_int[0];i++){
 			denom[i] = i+1;
 		}
-		int ret;
+		int ret=0;
 		//Case 1: Only N
 		if(index==1){
 			N = input_int[0];
@@ -41,14 +41,43 @@ int main(){
 			N = input_int[0];
 			L1 = input_int[1];
 			vector<vector<int> > DP(N+1,vector<int>(L1+1,0));
-			
+			DP[0][0] = 1;
+			//Fill the matrix
+			for(int d=0;d<N;d++){
+				for(int i=1;i<=N;i++){
+					for(int j=0;j<L1;j++){
+						if(i-denom[d]>=0){
+							DP[i][j+1]+=DP[i-denom[d]][j];
+						}
+					}
+				}
+			}
+			for(int i=0;i<=L1;i++){
+				ret+=DP[N][i];
+			}
 		}
 		//Case 3: N, L1, L2
 		else{
 			N = input_int[0];
 			L1 = input_int[1];
 			L2 = input_int[2];
+			vector<vector<int> > DP(N+1,vector<int>(L2+1,0));
+			DP[0][0] = 1;
+			//Fill the matrix
+			for(int d=0;d<N;d++){
+				for(int i=1;i<=N;i++){
+					for(int j=0;j<L2;j++){
+						if(i-denom[d]>=0){
+							DP[i][j+1]+=DP[i-denom[d]][j];
+						}
+					}
+				}
+			}
+			for(int i=L1;i<=L2;i++){
+				ret+=DP[N][i];
+			}
 		}
+		cout << ret << endl;
 	}
 
 	return 0;
