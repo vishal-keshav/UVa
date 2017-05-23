b1327079f74e4da5a7ce4dd21c808b70980ea746
bulletcross <bulletcross@gmail.com>
2017-05-23 21:12:15 +0530

    108 AC

--- Dynamic Programming/DP19 p108.cpp
+++ Dynamic Programming/DP19 p108.cpp
@@ -5,27 +5,44 @@
 
 using namespace std;
 
+#define INT_MAXIMUM 10000000
+
 int main(){
 	int N;
 	cin >> N;
-	vector<vector<int> > array(N,vector<int>(N));
+	vector<vector<int> > arr(N,vector<int>(N));
 	for(int i=0;i<N;i++){
 		for(int j=0;j<N;j++){
-			cin >> array[i][j];
+			cin >> arr[i][j];
 		}
 	}
-	vector<vector<int> > DP_sum(N,vector<int>(N));
-	DP_sum[0][0] = array[0][0];
-	for(int i=1;i<N;i++){
-		DP_sum[0][i] = DP_sum[0][i-1]+array[0][i];
-		DP_sum[i][0] = DP_sum[i][0]+array[i][0];
+
+	vector<vector<int> > DP_sum(N+1,vector<int>(N+1));
+	for(int i=0;i<=N;i++){
+		DP_sum[0][i] = 0;
+		DP_sum[i][0] = 0;
 	}
-	for(int i=1;i<N;i++){
-		for(int j=1;j<N;j++){
-			DP_sum[i][j] = DP_sum[i-1][j] + DP[i][j-1] + array[i][j] - DP_sum[i-1][j-1];
+
+	for(int i=0;i<N;i++){
+		for(int j=0;j<N;j++){
+			DP_sum[i+1][j+1] = DP_sum[i+1][j] + DP_sum[i][j+1] + arr[i][j] - DP_sum[i][j];
 		}
 	}
+	int temp_sum,ret=-INT_MAXIMUM;
 	//O(n^4) algo here
-	
+	for(int i=1;i<=N;i++){
+		for(int j=1;j<=N;j++){
+			for(int k=i;k<=N;k++){
+				for(int l=j;l<=N;l++){
+					temp_sum = DP_sum[k][l] - DP_sum[k][j-1] - DP_sum[i-1][l] + DP_sum[i-1][j-1];
+					if(temp_sum>ret){
+						ret = temp_sum;
+					}
+				}
+			}
+		}
+	}
+	cout << ret << endl;
+
 	return 0;
 }
