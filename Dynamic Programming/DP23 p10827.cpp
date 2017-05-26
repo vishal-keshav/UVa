f3cc5e6624ae537017bc9148dd2d61bec99a1cc1
bulletcross <bulletcross@gmail.com>
2017-05-26 23:07:03 +0530

    10827 AC

--- Dynamic Programming/DP23 p10827.cpp
+++ Dynamic Programming/DP23 p10827.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <algorithm>
 
+//#define DEBUG
+
 using namespace std;
 
 int main(){
@@ -17,63 +19,42 @@ int main(){
 			}
 		}
 		//For horizontal mapping
-		vector<vector<int> > horiz(N,vector<int>(2*N));
-		for(int i=0;i<N;i++){
-			for(int j=0;j<N;j++){
-				horiz[i][j] = arr[i][j];
-				horiz[i][j+N] = arr[i][j];
+		vector<vector<int> > arr_tourus(2*N,vector<int>(2*N));
+		for(int i=0;i<2;i++){
+			for(int j=0;j<2;j++){
+				for(int k=0;k<N;k++){
+					for(int l=0;l<N;l++){
+						arr_tourus[k+i*N][l+j*N] = arr[k][l];
+					}
+				}
 			}
 		}
-		vector<vector<int> > DP_sum_horiz(N+1,vector<int>(2*N+1,0));
-		for(int i=0;i<=N;i++){
-			DP_sum_horiz[i][0] = 0;
-		}
-		for(int i=0;i<=2*N;i++){
-			DP_sum_horiz[0][i] = 0;
-		}
-		for(int i=0;i<N;i++){
+#ifdef DEBUG
+		for(int i=0;i<2*N;i++){
 			for(int j=0;j<2*N;j++){
-				DP_sum_horiz[i+1][j+1] = horiz[i][j] + DP_sum_horiz[i][j+1] + DP_sum_horiz[i+1][j] - DP_sum_horiz[i][j];
+				cout << arr_tourus[i][j] << " ";
+			}
+			cout << endl;
+		}
+#endif
+		vector<vector<int> > DP_sum(2*N+1,vector<int>(2*N+1,0));
+
+		for(int i=0;i<=2*N;i++){
+			DP_sum[i][0] = 0;
+			DP_sum[0][i] = 0;
+		}
+
+		for(int i=0;i<2*N;i++){
+			for(int j=0;j<2*N;j++){
+				DP_sum[i+1][j+1] = arr_tourus[i][j] + DP_sum[i][j+1] + DP_sum[i+1][j] - DP_sum[i][j];
 			}
 		}
 		int temp,ret=-10000;
 		for(int i=1;i<=N;i++){
 			for(int j=1;j<=N;j++){
-				for(int k=i;k<=N;k++){
-					for(int l=j;l<j+N;l++){
-						temp = DP_sum_horiz[k][l] - DP_sum_horiz[k][j-1] - DP_sum_horiz[i-1][l] + DP_sum_horiz[i-1][j-1];
-						if(temp > ret){
-							ret = temp;
-						}
-					}
-				}
-			}
-		}
-		//For vertical mapping
-		vector<vector<int> > vert(2*N,vector<int>(N));
-		for(int i=0;i<N;i++){
-			for(int j=0;j<N;j++){
-				vert[i][j] = arr[i][j];
-				vert[i+N][j] = arr[i][j];
-			}
-		}
-		vector<vector<int> > DP_sum_vert(2*N+1,vector<int>(N+1,0));
-		for(int i=0;i<=N;i++){
-			DP_sum_vert[0][i] = 0;
-		}
-		for(int i=0;i<=2*N;i++){
-			DP_sum_vert[i][0] = 0;
-		}
-		for(int i=0;i<2*N;i++){
-			for(int j=0;j<N;j++){
-				DP_sum_vert[i+1][j+1] = vert[i][j] + DP_sum_vert[i][j+1] + DP_sum_vert[i+1][j] - DP_sum_vert[i][j];
-			}
-		}
-		for(int i=1;i<=N;i++){
-			for(int j=1;j<=N;j++){
 				for(int k=i;k<i+N;k++){
-					for(int l=j;l<=N;l++){
-						temp = DP_sum_vert[k][l] - DP_sum_vert[k][j-1] - DP_sum_vert[i-1][l] + DP_sum_vert[i-1][j-1];
+					for(int l=j;l<j+N;l++){
+						temp = DP_sum[k][l] - DP_sum[k][j-1] - DP_sum[i-1][l] + DP_sum[i-1][j-1];
 						if(temp > ret){
 							ret = temp;
 						}
