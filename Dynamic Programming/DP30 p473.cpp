f8316b55ae07385ab06ddfe9476e83af88239524
bulletcross <bulletcross@gmail.com>
2017-06-13 23:08:55 +0530

    473 AC

--- Dynamic Programming/DP30 p473.cpp
+++ Dynamic Programming/DP30 p473.cpp
@@ -2,17 +2,22 @@
 #include <stdio.h>
 #include <vector>
 #include <algorithm>
+#include <sstream>
+#include <string>
 
 using namespace std;
 
 int n,t,m;
 vector<int> input;
-
+vector<vector<vector<int> > > DP;
 
 int max_songs(int index, int current_disc_space, int disc_left){
     //cout << index << " " << current_disc_space << " " << disc_left << endl;
-	if(index>=n || disc_left<=0 || current_disc_space<=0){
+	if(index>=n || disc_left<=0){
 		return 0;
+	}
+	else if(DP[index][current_disc_space][disc_left]!=-1){
+        return DP[index][current_disc_space][disc_left];
 	}
 	else{
 		int ret = 0;
@@ -23,18 +28,46 @@ int max_songs(int index, int current_disc_space, int disc_left){
 		//Choice 2: dont take this song at all
 		ret = max(ret, max_songs(index+1,current_disc_space,disc_left));
 		//Choice 3: take the song in next disc
-		ret = max(ret, 1+max_songs(index+1,t-input[index],disc_left-1));
+		ret = max(ret, max_songs(index,t,disc_left-1));
+		DP[index][current_disc_space][disc_left] = ret;
 		return ret;
 	}
 }
 
 int main(){
-	cin >> n >> t >>m;
-	input.resize(n);
-	for(int i=0;i<n;i++){
-		cin >> input[i];
+	int T,temp;
+	int nr_case = 1;
+	string input_str;
+	cin >> T;
+	cin.ignore();
+	while(T--){
+		cin >> n >> t >> m;
+		//cin.ignore();
+		cin.ignore();
+		//cin >> input_str;
+		getline(cin,input_str);
+        //cout << input_str << endl;
+		input.clear();
+		stringstream ss(input_str);
+		while(ss >> temp){
+			input.push_back(temp);
+			if(ss.peek()==','){
+				ss.ignore();
+			}
+		}
+		/*for(int i=0;i<n;i++){
+			cin >> input[i];
+		}*/
+		DP.clear();
+		DP.resize(n,vector<vector<int> >(t+1,vector<int>(m+1,-1)));
+		cout << max_songs(0,t,m) << endl;
+		if(T){
+			cout << endl;
+		}
+		/*if(nr_case==44){
+            cout << input_str << endl;
+		}*/
+		nr_case++;
 	}
-	cout << max_songs(0,t,m) << endl;
-
 	return 0;
 }
