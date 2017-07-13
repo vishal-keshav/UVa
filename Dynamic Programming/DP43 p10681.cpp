69162109f2caa4d299742706f6b63801aa706fdc
bulletcross <bulletcross@gmail.com>
2017-07-13 23:02:18 +0530

    Probability handbook

--- Dynamic Programming/DP43 p10681.cpp
+++ Dynamic Programming/DP43 p10681.cpp
@@ -8,6 +8,7 @@ using namespace std;
 int C,L;
 int S,E,D;
 vector<vector<int> > link;
+vector<vector<bool> > DP;
 
 int main(){
 	int c1, c2;
@@ -20,7 +21,14 @@ int main(){
 		link[c2-1][c1-1] = 1;
 	}
 	cin >> S >> E >> D;
-	
+	DP.clear();
+	DP.resize(C,vector<bool>(D+1,false));
+	DP[S-1][0] = true;
+	for(int i=1;i<=D;i++){
+		for(int j=0;j<C;j++){
+			
+		}
+	}
 	
 	return 0;
 }
