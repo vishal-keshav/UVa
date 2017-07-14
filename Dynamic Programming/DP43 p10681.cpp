f15b0618eba85702700310d290b2786714fd2c34
bulletcross <bulletcross@gmail.com>
2017-07-14 21:40:26 +0530

    10681 AC

--- Dynamic Programming/DP43 p10681.cpp
+++ Dynamic Programming/DP43 p10681.cpp
@@ -7,28 +7,42 @@ using namespace std;
 
 int C,L;
 int S,E,D;
-vector<vector<int> > link;
+vector<vector<bool> > link;
 vector<vector<bool> > DP;
 
 int main(){
 	int c1, c2;
 	cin >> C >> L;
-	link.clear();
-	link.resize(C,vector<int>(C,0));
-	for(int i=0;i<L;i++){
-		cin >> c1 >> c2;
-		link[c1-1][c2-1] = 1;
-		link[c2-1][c1-1] = 1;
+	while(C+L){
+        link.clear();
+        link.resize(C,vector<bool>(C,false));
+        for(int i=0;i<L;i++){
+            cin >> c1 >> c2;
+            link[c1-1][c2-1] = true;
+            link[c2-1][c1-1] = true;
+        }
+        cin >> S >> E >> D;
+        DP.clear();
+        DP.resize(C,vector<bool>(D+1,false));
+        DP[S-1][0] = true;
+        for(int i=1;i<=D;i++){
+            for(int j=0;j<C;j++){
+                if(DP[j][i-1]){
+                    for(int k=0;k<C;k++){
+                        if(k!=j && link[j][k]){
+                            DP[k][i] = true;
+                        }
+                    }
+                }
+            }
+        }
+        if(DP[E-1][D]){
+            cout << "Yes, Teobaldo can travel." << endl;
+        }
+        else{
+            cout  << "No, Teobaldo can not travel." << endl;
+        }
+        cin >> C >> L;
 	}
-	cin >> S >> E >> D;
-	DP.clear();
-	DP.resize(C,vector<bool>(D+1,false));
-	DP[S-1][0] = true;
-	for(int i=1;i<=D;i++){
-		for(int j=0;j<C;j++){
-			
-		}
-	}
-	
 	return 0;
 }
