fa314a354499c8f6487b698a3a6776f16ead7932
bulletcross <bulletcross@gmail.com>
2017-07-15 12:33:10 +0530

    4160 continued, going out :(

---
+++ Dynamic Programming/DP45 p4106.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int c,t,h,f;
+
+int main(){
+	cin >> c
+	while(c--){
+		cin >> t >> h >> f;
+		for(int i=0;i<t;i++){
+			
+		}
+		
+		
+	}
+	cin >> c;
+	return 0;
+}
