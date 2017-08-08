67549e83c7376c3be342517894b8cf564f9147e5
bulletcross <bulletcross@gmail.com>
2017-08-08 23:09:02 +0530

    782 incomplete

--- Graph/GR7 p782.cpp
+++ Graph/GR7 p782.cpp
@@ -4,20 +4,58 @@
 #include <vector>
 #include <string>
 
-#define DEBUG 1
+//#define DEBUG 1
 
 using namespace std;
 
-int T;
+int T,h,w;
 vector<string> contour;
+
+int move_x[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
+int move_y[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
 
 int maximum(int a, int b){
     return (a<b?b:a);
 }
 
+bool is_valid(int x, int y){
+	if(x>=0 && y>=0 && x<h && y<w){
+		return true;
+	}
+	return false;
+}
+
+/*Proposed algorithm
+1. Search for the star
+2. From that location, flood fill spaces and star with star
+3. Apply boundry condition to convert star to hash
+4. Convert star to spaces*/
+
+void flood_fill_star(int i, int j){
+	if(!is_valid(i,j)){
+		return;
+	}
+	if(contour[i][j]==' '){
+		contour[i][j] = '*';
+		for(int i=0;i<8;i++){
+			flood_fill_star(i+move_x[i], j+move_y[i]);
+		}
+	}
+	return;
+}
+
+void apply_boundry(void){
+	for(int i=0;i<h;i++){
+		for(int j=0;j<w;j++){
+			if(contour[i][j]=='X'){
+				//Convert closest * to #
+			}
+		}
+	}
+}
+
 int main(){
 	string temp;
-	int h,w;
 	cin >> T;
 	while(T--){
 		contour.clear();
@@ -41,6 +79,28 @@ int main(){
 			cout << contour[i] << endl;
 		}
 #endif
+		for(int i=0;i<h;i++){
+			for(int j=0;j<w;j++){
+				if(contour[i][j]=='*'){
+					contour[i][j] = ' ';
+					flood_fill_star(i,j);
+					break;//Can be optimized more
+				}
+			}
+		}
+		apply_boundry();
+		for(int i=0;i<h;i++){
+			for(int j=0;j<w;j++){
+				if(contour[i][j]=='*'){
+					contour[i][j] = ' ';
+				}
+			}
+		}
+		//Return answer
+		for(int i=0;i<h;i++){
+			cout << contour[i] << endl;
+		}
+		cout << temp << endl;
 	}
 
 	return 0;
