b71d63054216d1245414b338b448276b5c8e998b
bulletcross <bulletcross@gmail.com>
2018-01-24 22:24:51 +0530

    11545 WA

--- Graph/GR51 p11545.cpp
+++ Graph/GR51 p11545.cpp
@@ -8,22 +8,44 @@ using namespace std;
 #define INF 10000000
 string path;
 
-int compute_time(int loc, int time_cost, int time, int walked){
+vector<vector<vector<int> > > memo;
+
+int compute_time(int loc, int time, int walked){
+    if(memo[loc][time][walked]!=-1){
+        return memo[loc][time][walked];
+    }
 	if(path[loc]=='D'){
-		return time_cost;
+        memo[loc][time][walked] = 0;
+		return 0;
 	}
-	else{
-		
+    if (((time <= 6 || time >= 18) && path[loc] == '*')||(walked > 16)){
+        memo[loc][time][walked] = INF;
+        return INF;
 	}
+    int time_cost = 1 + compute_time(loc+1, (time+1)%24, walked+1);
+	int sleep_cost = 9 + compute_time(loc+1, (time+9)%24, 1);
+	time_cost = min(time_cost, sleep_cost);
+	memo[loc][time][walked] = time_cost;
+	return time_cost;
 }
 
 int main(){
+    //freopen("output.txt","w",stdout);
 	int T,nr_case=1;
 	cin >> T;
 	while(T--){
 		cin >> path;
-		cout << "Case #" << nr_case << ": " << compute_time(1, 1, 6, 1) << endl;
+		memo.clear();
+		memo.resize(path.size()+1, vector<vector<int> >(25, vector<int>(25,-1)));
+		int res = compute_time(0, 6, 0);
+		if(res >= INF){
+            cout << "Case #" << nr_case << ": " << -1 << endl;
+		}
+		else{
+            cout << "Case #" << nr_case << ": " << res << endl;
+		}
+		nr_case++;
 	}
-	
+
 	return 0;
 }
