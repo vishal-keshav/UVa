afa4befa9d5d4476b3327dbebd66764e3653ccf9
bulletcross <bulletcross@gmail.com>
2018-01-25 21:28:39 +0530

    adding file

---
+++ Graph/GR52 p11730.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int S,T;
+
+int main(){
+	int nr_case = 1;
+	cin >> S >> T;
+	while(S+T){
+		
+		
+		cin >> S >> T;
+	}
+	return 0;
+}
