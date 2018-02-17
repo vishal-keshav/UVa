a1096bd8a357e31f27bc041f997ebc7d8c930002
bulletcross <bulletcross@gmail.com>
2018-02-17 20:07:16 +0530

    11733 AC

--- Graph/GR64 p11733.cpp
+++ Graph/GR64 p11733.cpp
@@ -16,13 +16,14 @@ struct edge{
 vector<struct edge> edges;
 
 vector<int> union_set;
-
+int nr_sets;
 void init_uf(void){
 	union_set.clear();
 	union_set.resize(N);
 	for(int i=0;i<N;i++){
 		union_set[i] = i;
 	}
+	nr_sets = N;
 }
 int find_set(int n){
 	if(n==union_set[n]){
@@ -40,10 +41,29 @@ void make_union(int n1, int n2){
 	if(!is_sameset(n1,n2)){
 		union_set[find_set(n1)] = find_set(n2);
 	}
+	nr_sets--;
 }
 
 void compute(void){
-	
+	int temp_cost = A*N;
+	min_cost = A*N;
+	nr_airport = N;
+	init_uf();
+	auto sort_fn = [](struct edge e1, struct edge e2) -> bool{
+		return (e1.w < e2.w);
+	};
+	sort(edges.begin(), edges.end(), sort_fn);
+	for(int i=0;i<M;i++){
+		if(!is_sameset(edges[i].e.first, edges[i].e.second)){
+			make_union(edges[i].e.first, edges[i].e.second);
+			temp_cost = temp_cost - A + edges[i].w;
+			if(temp_cost < min_cost){
+				min_cost = temp_cost;
+				nr_airport = nr_sets;
+			}
+		}
+	}
+	return;
 }
 
 int main(){
@@ -56,10 +76,13 @@ int main(){
 		edges.clear();
 		for(int i=0;i<M;i++){
 			cin >> temp.e.first >> temp.e.second >> temp.w;
+			temp.e.first--;
+			temp.e.second--;
 			edges.push_back(temp);
 		}
 		compute();
 		cout << "Case #" << nr_case << ": " << min_cost << " " << nr_airport << endl;
+		nr_case++;
 	}
 
 	return 0;
