1c2e9eb4ed35f9b67ec1e51811239a59c9d7930a
bulletcross <bulletcross@gmail.com>
2018-02-25 16:48:28 +0530

    10397 WA

--- Graph/GR68 p10397.cpp
+++ Graph/GR68 p10397.cpp
@@ -3,25 +3,25 @@
 #include <algorithm>
 #include <vector>
 #include <utility>
+#include <cmath>
 #include <map>
 
 using namespace std;
 
-int T,N,M;
+int N,M;
 map<pair<int, int>, int > vertex_map;
 vector<pair<int, int> > vertex_list;
 struct edge{
 	int n1,n2;
-	int w;
+	float w;
 };
 vector<struct edge> edges;
 
-int dist(pair<int, int> p1, pair<int, int> p2){
-	int ret = (p1.first - p2.first)*(p1.first - p2.first);
-	ret+= (p1.second - p2.second)*(p1.second - p2.second);
-	return ret;
+float dist(pair<int, int> p1, pair<int, int> p2){
+	float ret = (float)((p1.first - p2.first)*(p1.first - p2.first));
+	ret+= (float)((p1.second - p2.second)*(p1.second - p2.second));
+	return sqrt(ret);
 }
-
 //////////////////////////////////
 vector<int> union_set;
 int nr_sets;
@@ -52,23 +52,19 @@ void make_union(int n1, int n2){
 	nr_sets--;
 }
 ////////////////////////////////////////
-vector<pair<int, int> > compute(void){
-	vector<pair<int, int> > ret;
+float compute(void){
 	init_uf(N);
 	auto sort_fn = [](struct edge e1, struct edge e2) -> bool {
 		return (e1.w < e2.w);
 	};
 	sort(edges.begin(), edges.end(), sort_fn);
 	int index = 0;
+	float ret = 0.0;
 	pair<int, int> temp;
 	while(nr_sets!=1){
 		if(!is_sameset(edges[index].n1, edges[index].n2)){
 			make_union(edges[index].n1, edges[index].n2);
-			if(edges[index].w!=0){
-				temp.first = edges[index].n1;
-				temp.second = edges[index].n2;
-				ret.push_back(temp);
-			}
+			ret+=edges[index].w;
 		}
 		index++;
 	}
@@ -77,12 +73,10 @@ vector<pair<int, int> > compute(void){
 
 int main(){
     //freopen("output.txt","w",stdout);
-	cin >> T;
-	while(T--){
+	while(cin >> N){
 		pair<int, int> temp;
 		vertex_map.clear();
 		vertex_list.clear();
-		cin >> N;
 		for(int i=0;i<N;i++){
 			cin >> temp.first >> temp.second;
 			vertex_list.push_back(temp);
@@ -107,18 +101,7 @@ int main(){
 			temp_edge.w = 0;
 			edges.push_back(temp_edge);
 		}
-		vector<pair<int, int> > ans = compute();
-		if(ans.size()==0){
-			cout << "No new highways need" << endl;
-		}
-		else{
-			for(int i=0;i<ans.size();i++){
-				cout << ans[i].first+1 << " " << ans[i].second+1 << endl;
-			}
-		}
-		if(T){
-			cout << endl;
-		}
+		printf("%0.2f\n", compute());
 	}
 	return 0;
 }
